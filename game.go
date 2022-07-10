301ee675ab21ddf613a3b83993b6c58e545469d6
Duy Nguyen <duy210697@gmail.com>
2022-07-10 18:34:38 +0700

    add music and camera

--- game.go
+++ game.go
@@ -16,11 +16,16 @@ var (
 	playerDest   rl.Rectangle
 
 	playerSpeed float32 = 3
+
+	musicPaused bool
+	music       rl.Music
+
+	cam rl.Camera2D
 )
 
 func drawScene() {
 	rl.DrawTexture(grassSprite, 100, 50, rl.White)
-	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(0, 0), 0, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
 }
 
 func input() {
@@ -36,18 +41,33 @@ func input() {
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
 		playerDest.X += playerSpeed
 	}
+
+	if rl.IsKeyPressed(rl.KeyQ) {
+		musicPaused = !musicPaused
+	}
 }
 
 func update() {
 	running = !rl.WindowShouldClose()
+
+	rl.UpdateMusicStream(music)
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	}
+
+	cam.Target = rl.NewVector2(float32(playerDest.X-playerDest.Width/2), float32(playerDest.Y-playerDest.Height/2))
 }
 
 func render() {
 	rl.BeginDrawing()
 	rl.ClearBackground(bgColor)
+	rl.BeginMode2D(cam)
 
 	drawScene()
 
+	rl.EndMode2D()
 	rl.EndDrawing()
 }
 
@@ -61,11 +81,25 @@ func initialize() {
 
 	playerSrc = rl.NewRectangle(0, 0, 48, 48)
 	playerDest = rl.NewRectangle(100, 100, 100, 100)
+
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("res/music.mp3")
+	musicPaused = false
+	rl.PlayMusicStream(music)
+
+	cam = rl.NewCamera2D(
+		rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
+		rl.NewVector2(float32(playerDest.X-playerDest.Width/2), float32(playerDest.Y-playerDest.Height/2)),
+		0,
+		1.0,
+	)
 }
 
 func quit() {
 	rl.UnloadTexture(grassSprite)
 	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
 	rl.CloseWindow()
 }
 
